2ffee69ee699ba897d8fd4806ef9df49d3f28104
MJ Baysic <mjpb123@gmail.com>
2024-07-17 16:27:37 -0400

    neynar

--- src/app/api/[[...routes]]/route.tsx
+++ src/app/api/[[...routes]]/route.tsx
@@ -1,18 +1,24 @@
 /** @jsxImportSource frog/jsx */
-import { Button, Frog, TextInput } from "frog";
+import { Button, Frog, TextInput, TextInputProps } from "frog";
 import { handle } from "frog/next";
 import { devtools } from "frog/dev";
 import { serveStatic } from "frog/serve-static";
 import { pinata } from "frog/hubs";
+import { neynar } from "frog/middlewares";
 
 const app = new Frog({
   basePath: "/api",
   title: "Frog",
   hub: pinata(),
-});
+}).use(
+  neynar({
+    apiKey: "NEYNAR_FROG_FM",
+    features: ["interactor", "cast"],
+  })
+);
 
 app.frame("/", (c) => {
-  const { buttonValue, status, frameData, verified, inputText } = c;
+  const { buttonValue, status, frameData, inputText } = c;
 
   return c.res({
     image: (
@@ -31,7 +37,41 @@ app.frame("/", (c) => {
       >
         {status === "initial"
           ? "Select your fruit!"
-          : `${frameData?.fid.toString()} Selected ${buttonValue} and typed in "${inputText}"`}
+          : `${frameData?.fid.toString()} selected ${buttonValue}`}
+      </div>
+    ),
+    intents: [
+      <Button value="apple">Apple</Button>,
+      <Button value="banana">Banana</Button>,
+      <Button value="mango">Mango</Button>,
+      <Button action="/neynar">Neynar</Button>,
+    ],
+  });
+});
+
+app.frame("/neynar", (c) => {
+  const { buttonValue, status, inputText } = c;
+  const { displayName, followerCount, pfpUrl } = c.var.interactor || {};
+
+  return c.res({
+    image: (
+      <div
+        style={{
+          color: "white",
+          display: "flex",
+          fontSize: 60,
+          alignItems: "center",
+          flexDirection: "column",
+          justifyContent: "center",
+          textAlign: "center",
+          height: "100%",
+          width: "100%",
+        }}
+      >
+        <img src={pfpUrl} style={{ width: 200, height: 200 }} />
+        {status === "initial"
+          ? "Select your fruit!"
+          : `${displayName} selected ${buttonValue}`}
       </div>
     ),
     intents: [
